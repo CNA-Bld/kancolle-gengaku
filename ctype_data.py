67ff7cb205c2b45a465b5d34736ed986a808fae3
CNA-Bld <cna.bld@gmail.com>
2017-10-08 22:32:48 +0800

    now named nedb not json

--- ctype_data.py
+++ ctype_data.py
@@ -7,12 +7,12 @@ data = {}
 
 classes = {}
 
-with open(JSON_DB_PATH + 'ship_classes.json', 'r', encoding='utf-8') as f:
+with open(JSON_DB_PATH + 'ship_classes.nedb', 'r', encoding='utf-8') as f:
     for line in f.readlines():
         this_class = json.loads(line)
         classes[this_class['id']] = this_class['name_game']
 
-with open(JSON_DB_PATH + 'ships.json', 'r', encoding='utf-8') as f:
+with open(JSON_DB_PATH + 'ships.nedb', 'r', encoding='utf-8') as f:
     for line in f.readlines():
         ship = json.loads(line)
         data[str(ship['id'])] = {'id': str(ship['id']), 'name': ship['name']['ja_jp'], 'ctype': classes.get(ship['class'], ''),
