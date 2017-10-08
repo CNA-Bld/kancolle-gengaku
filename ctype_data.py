2b5924519510f96c76534778bef5448a478e827f
CNA-Bld <cna.bld@gmail.com>
2017-10-08 22:34:57 +0800

    now named ['name']['ja_jp'] for ship_classes

--- ctype_data.py
+++ ctype_data.py
@@ -10,7 +10,7 @@ classes = {}
 with open(JSON_DB_PATH + 'ship_classes.nedb', 'r', encoding='utf-8') as f:
     for line in f.readlines():
         this_class = json.loads(line)
-        classes[this_class['id']] = this_class['name_game']
+        classes[this_class['id']] = this_class['name']['ja_jp']
 
 with open(JSON_DB_PATH + 'ships.nedb', 'r', encoding='utf-8') as f:
     for line in f.readlines():
